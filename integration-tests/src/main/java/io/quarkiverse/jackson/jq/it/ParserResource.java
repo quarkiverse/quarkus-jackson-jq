fbcada607c9eb98791f94a177ae1741badff1781
Helber Belmiro <helber.belmiro@gmail.com>
2023-02-15 08:27:15 -0300

    Upgraded to Quarkus 3
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- integration-tests/src/main/java/io/quarkiverse/jackson/jq/it/ParserResource.java
+++ integration-tests/src/main/java/io/quarkiverse/jackson/jq/it/ParserResource.java
@@ -21,13 +21,13 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
